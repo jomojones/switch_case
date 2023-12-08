db154e5ba35ca7303180440aa2e85fea8457de1a
jomo <mboywinthmbigent@gmail.com>
2023-12-08 13:19:07 +0300

    i have added switch case

---
+++ app.js
@@ -0,0 +1,174 @@
+/*const employees =[
+    {name:'denis', Age:20, Salary:2000},
+    {name:'khushi', Age:30, Salary:3000},
+    {name:'John', Age:40, Salary:4000},
+    {name:'Ligare', Age:50, Salary:6000},
+    {name:'Kevin', Age:70, Salary:9000}
+]
+//higher order function
+function allsalaries(allparameter){
+    console.log(allparameter.Age)
+}
+
+let all = employees.forEach(allsalaries)
+
+let firstname="Jomo"
+let last_name="Jones"
+let address="mainstreet"
+address="first_street"
+
+console.log(firstname,last_name,address)
+
+let street= "Hurlingam"
+let country= "Jamaica"
+
+let fullMailingAddress= street + " "+country
+
+console.log(fullMailingAddress)
+
+let score1=20
+let score2=50
+let score3=90
+
+let totalscore=score1+score2+score3
+let averagescore=totalscore/3
+
+console.log(totalscore)
+console.log(averagescore)
+
+let plates=20
+let people=7
+
+let remaining_plates=plates-people
+remaining_plates++
+console.log(`There are ${remaining_plates} plates available`)
+
+let fruits =[
+    "oranges",
+    "watermelons",
+    "oranges",
+    "Santara",
+    "Grapes",
+    20,
+
+]
+
+const first_fruit=fruits[0]
+fruits[3]='lemon'
+console.log(first_fruit,fruits)
+
+function calculateTotal(subTotal,tax){
+
+sum=subTotal+tax
+
+return sum
+
+
+}
+let car ={
+    make:"Wish",
+    model:"Toyota",
+    year:2001,
+    colours:["red","blue","black","green","yellow"],
+    hybrid:"yes/no",
+    drive:function(){
+
+        console.log("random text")
+    },
+    stop:function(){
+
+        console.log("more random text")
+    }
+
+
+}
+console.log(car.make)
+console.log(car.colours[0])
+console.log(car.drive())
+console.log(car.stop())
+
+let age=16
+if (age>=18) {
+    console.log("you are an adult")
+} else {
+    console.log("you are a child")
+
+}
+
+let person1={
+age:25,
+_status:"Tourist",
+
+}
+let person2={
+age:17,
+_status:"resident",
+
+}
+
+if(person1.age>18 && person1._status==="resident"){
+
+console.log("yes, you passed!")
+}else{
+    console.log("no, you failed!")
+}
+
+let age=16
+let fullname="Hogan"
+
+if (age==16 && fullname=="Hogan"){
+    console.log("You qualify to join the next class.")
+} else {
+console.log("NO!")
+}*/
+ let dice=0
+
+ switch(dice){
+    case 1:
+        console.log("You got one")
+       break;
+    case 2:
+        console.log("you got two") 
+       break;
+    case 3:
+        console.log("You got three")
+        break;   
+    case 4:
+        console.log("you got four")  
+        break;
+    case 5:
+        console.log("You got five")
+         break;      
+    case 6:
+        console.log("you got six")
+          break;
+    default:
+        console.log("Try again.")      
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
